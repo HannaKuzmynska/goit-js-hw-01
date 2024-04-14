28d3eb7dd98e069152f9d838f7abd428b5e11f45
Hanna Kuzmynska <kuzminskaiaann@gmail.com>
2024-04-14 20:39:22 +0300

    add parseFloat

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth (content, padding, border) {
-    const contentWidth = parseInt (content); 
+    const contentWidth = parseFloat (content); 
     const paddingWidth = parseFloat (padding);
     const borderWidth = parseFloat (border);
     const totalWidth = contentWidth + (paddingWidth + borderWidth)*2;
